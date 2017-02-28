72fe104b56f4123b5d7ed2ff8d75027b55e42981
Andre Gebers <a.gebers@uq.edu.au>
2017-02-28 11:28:43 +1000

    also add the interview transcript to the search 'all' field

--- src/net/qldarch/search/update/UpdateArchObjJob.java
+++ src/net/qldarch/search/update/UpdateArchObjJob.java
@@ -1,11 +1,15 @@
 package net.qldarch.search.update;
 
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.lucene.index.IndexWriter;
 
 import lombok.AllArgsConstructor;
 import net.qldarch.archobj.ArchObj;
+import net.qldarch.interview.Interview;
+import net.qldarch.interview.Utterance;
 
 @AllArgsConstructor
 public class UpdateArchObjJob extends CancelableIndexUpdateJob {
@@ -20,6 +24,14 @@ public class UpdateArchObjJob extends CancelableIndexUpdateJob {
           new DeleteDocumentJob(archobj.getId().toString(), archobj.getType().toString()).run(writer);
           Map<String, Object> m = archobj.asMap();
           m.put("category", "archobj");
+          if(archobj instanceof Interview) {
+            final Set<Utterance> transcript = ((Interview)archobj).getTranscript();
+            if(transcript != null) {
+              final String text = transcript.stream().map(
+                  Utterance::getTranscript).collect(Collectors.joining(" "));
+              m.put("all", text);
+            }
+          }
           writer.addDocument(DocumentUtils.createDocument(m));
         }
       } catch(Exception e) {

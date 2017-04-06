751bf7b142c0bf757946b76bf3066c1179cb5305
Friska (Dendi) Pambudi <f.pambudi@uq.edu.au>
2017-04-06 14:11:25 +1000

    added service to update appuser

---
+++ src/net/qldarch/security/WsUpdateUser.java
@@ -0,0 +1,72 @@
+package net.qldarch.security;
+
+import java.util.Map;
+
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
+import lombok.extern.slf4j.Slf4j;
+import net.qldarch.hibernate.HS;
+import net.qldarch.jaxrs.ContentType;
+import net.qldarch.util.ObjUtils;
+import net.qldarch.util.UpdateUtils;
+
+@Path("/account/update")
+@Slf4j
+public class WsUpdateUser {
+
+  @Inject
+  @Nullable
+  private User user;
+
+  @Inject
+  private UserStore users;
+
+  @Inject
+  private HS hs;
+
+  @POST
+  @Path("/{id}")
+  @Produces(ContentType.JSON)
+  public Response update(@PathParam("id") Long id, MultivaluedMap<String, Object> params) {
+    final User u = users.get(id);
+    if(u == null) {
+      log.debug("account with id {} does not exist", id);
+      Response.status(404).build();
+    } else {
+      if(user != null) {
+        if(user.isAdmin()) {
+          final Map<String, Object> m = UpdateUtils.asMap(params);
+          if(m.containsKey("contact")) {
+            u.setContact(ObjUtils.asBoolean(m.get("contact")));
+          }
+          if(m.containsKey("displayName")) {
+            u.setDisplayName(ObjUtils.asString(m.get("displayName")));
+          }
+          if(m.containsKey("email")) {
+            u.setEmail(ObjUtils.asString(m.get("email")));
+          }
+          if(m.containsKey("role")) {
+            u.setRole(ObjUtils.asString(m.get("role")));
+          }
+          if(m.containsKey("username")) {
+            u.setUsername(ObjUtils.asString(m.get("username")));
+          }
+          hs.update(u);
+          log.info("updating of appuser id {} successful", id);
+          return Response.ok().build();
+        }
+      } else {
+        Response.status(404).build();
+      }
+    }
+    return Response.status(403).build();
+  }
+
+}
